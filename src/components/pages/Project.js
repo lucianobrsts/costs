9b06b8145a5089b723e1b395fde241d8d9b6463f
luciano.brito <luciano.brito@boavistapartners.com.br>
2024-10-17 08:53:30 -0300

    Cria area de serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -13,6 +13,7 @@ function Project() {
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -33,6 +34,8 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
+
         //budget validation
         if(project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
@@ -60,6 +63,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -98,6 +105,22 @@ function Project() {
                                 </div>
                             )}
                         </div>
+
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+                            </button>
+
+                            <div className={styles.project_info}>
+                            {showServiceForm && <div>formulário do serviço</div>}
+                            </div>
+                        </div>
+
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>Itens de Serviços</p>
+                        </Container>
                     </Container>
                 </div>
             ) : (
